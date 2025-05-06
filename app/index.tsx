8e61125d01034ebb3560b5f4d2d9a2650955e685
daniel2825 <daniel2825@hotmail.com>
2025-05-05 20:28:23 -0500

    update

--- app/index.tsx
+++ app/index.tsx
@@ -5,6 +5,13 @@ import { Amplify } from "aws-amplify";
 import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react-native";
 import Home from "./Home/index";
 
+// https://docs.amplify.aws/react-native/build-a-backend/auth/set-up-auth/
+// https://blog.logrocket.com/aws-amplify-react-native-tutorial-examples/
+// https://ui.docs.amplify.aws/react/connected-components/authenticator/customization
+// https://genotechies.medium.com/expo-and-aws-amplify-auth-error-an-unknown-error-has-occurred-77b361620ae7
+// https://docs.amplify.aws/react-native/start/quickstart/
+// https://www.youtube.com/watch?v=SF4K1kDmHwU
+
 import outputs from "../amplify_outputs.json";
 
 Amplify.configure(outputs);
