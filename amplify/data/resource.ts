391bf757b6bcfaf828e8613a5e75b7f025b16488
daniel2825 <daniel2825@hotmail.com>
2026-04-13 20:33:06 -0500

    Add fields successful for dynamodb table and consult information in the future

--- amplify/data/resource.ts
+++ amplify/data/resource.ts
@@ -1,11 +1,20 @@
 import { type ClientSchema, a, defineData } from '@aws-amplify/backend';
 
 const schema = a.schema({
+
+  Videos: a.customType({
+        module_number: a.string(),
+        module_title: a.string(),
+        content: a.string(),
+        banner_video: a.string()
+      }),
   SongCourseContent: a
     .model({
       content: a.string(),
       title: a.string(),
       banner_image: a.string(),
+      videos: a.ref('Videos').array()
+      
     })
     .authorization((allow) => [allow.guest()]),
 });
